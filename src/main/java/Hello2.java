792546c766658ba9914969c0ab4726a977249626
y00111592 <y00111592@163.com>
2017-01-07 23:58:52 +0800

    首次提交

---
+++ src/main/java/Hello2.java
@@ -0,0 +1,18 @@
+/**
+ * Created by YLS on 2017/1/7.
+ */
+public class Hello2 {
+    private String name = "";
+
+    public String getName() {
+        return  this.name;
+    }
+
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello2");
+    }
+}
---
+++ src/test/java/Hello2Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * Created by YLS on 2017/1/7.
+ */
+public class Hello2Test {
+    @Test
+    public void test() {
+        Hello2 hello2 = new Hello2();
+        hello2.setName("00111592");
+        System.out.println(hello2.getName());
+    }
+
+}
